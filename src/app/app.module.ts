0a98655340a727c3063fb1d4d990845c350dbfc6
NoLogig <max-schweigerdt@web.de>
2018-10-27 20:29:45 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatFormFieldModule, MatButtonModule, MatCardModule, MatInputModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
